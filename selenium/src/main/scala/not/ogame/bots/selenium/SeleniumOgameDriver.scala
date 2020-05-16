45da7ae261a188be485aac9efcca84d2f47e03b9
ghostbuster91 <kghost0@gmail.com>
2020-05-17 00:13:59 +0200

    Wrap every single webdriver command with IO

--- selenium/src/main/scala/not/ogame/bots/selenium/SeleniumOgameDriver.scala
+++ selenium/src/main/scala/not/ogame/bots/selenium/SeleniumOgameDriver.scala
@@ -1,40 +1,43 @@
 package not.ogame.bots.selenium
 
 import cats.effect.IO
-import not.ogame.bots.selenium.WebDriverUtils.WebDriverImprovements
 import not.ogame.bots.{Credentials, OgameDriver, PlanetFactories}
 import org.openqa.selenium.{By, WebDriver}
 
 import scala.jdk.CollectionConverters._
+import cats.implicits._
+import WebDriverSyntax._
 
-class SeleniumOgameDriver(private val credentials: Credentials, private val webDriver: WebDriver) extends OgameDriver[IO] {
+class SeleniumOgameDriver(credentials: Credentials)(implicit webDriver: WebDriver) extends OgameDriver[IO] {
 
-  override def login(): IO[Unit] =
-    IO.delay(() -> {
-      val url = "https://lobby.ogame.gameforge.com/pl_PL/accounts"
-      webDriver.get(url)
-      if (!webDriver.findElements(By.id("loginRegisterTabs")).isEmpty) {
-        insetCredentials()
-        webDriver.get(url)
+  override def login(): IO[Unit] = {
+    val url = "https://lobby.ogame.gameforge.com/pl_PL/accounts"
+    for {
+      _                 <- go to url
+      loginRegisterTabs <- findMany(By.id("loginRegisterTabs"))
+      _ <- if (loginRegisterTabs.nonEmpty) {
+        insetCredentials() >> (go to url)
+      } else {
+        IO.unit
       }
-      selectUniverse()
-      webDriver.switchToOtherTab()
-      webDriver.waitForElement(By.className("OGameClock"))
-      webDriver.close()
-      webDriver.switchToAnyOpenTab()
-    })
-
-  private def insetCredentials() {
-    webDriver.findElement(By.id("loginRegisterTabs")).findElement(By.className("tabsList")).findElement(By.tagName("li")).click()
-    webDriver.findElement(By.name("email")).sendKeys(credentials.login)
-    webDriver.findElement(By.name("password")).sendKeys(credentials.password)
-    Thread.sleep(700)
-    webDriver.findElement(By.className("button-lg")).click()
-    webDriver.waitForElement(By.id("joinGame"))
+      _ <- selectUniverse()
+      _ <- switchToOtherTab()
+      _ <- waitForElement(By.className("OGameClock"))
+      _ <- IO.delay(webDriver.close())
+      _ <- switchToAnyOpenTab()
+    } yield ()
   }
 
-  private def selectUniverse() {
-    webDriver.waitForElement(By.className("rt-tr"))
+  private def insetCredentials(): IO[Unit] =
+    IO.delay {
+      webDriver.findElement(By.id("loginRegisterTabs")).findElement(By.className("tabsList")).findElement(By.tagName("li")).click()
+      webDriver.findElement(By.name("email")).sendKeys(credentials.login)
+      webDriver.findElement(By.name("password")).sendKeys(credentials.password)
+      Thread.sleep(700)
+      webDriver.findElement(By.className("button-lg")).click()
+    } >> waitForElement(By.id("joinGame")).void
+
+  private def selectUniverse(): IO[Unit] = waitForElement(By.className("rt-tr")) >> IO.delay {
     webDriver
       .findElements(By.className("rt-tr"))
       .asScala
--- selenium/src/main/scala/not/ogame/bots/selenium/SeleniumOgameDriverCreator.scala
+++ selenium/src/main/scala/not/ogame/bots/selenium/SeleniumOgameDriverCreator.scala
@@ -2,8 +2,11 @@ package not.ogame.bots.selenium
 
 import cats.effect.IO
 import not.ogame.bots.{Credentials, OgameDriver, OgameDriverCreator}
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 
 class SeleniumOgameDriverCreator extends OgameDriverCreator[IO] {
-  override def create(credentials: Credentials): OgameDriver[IO] = new SeleniumOgameDriver(credentials, new FirefoxDriver())
+  private implicit val driver: WebDriver = new FirefoxDriver()
+
+  override def create(credentials: Credentials): OgameDriver[IO] = new SeleniumOgameDriver(credentials)
 }
