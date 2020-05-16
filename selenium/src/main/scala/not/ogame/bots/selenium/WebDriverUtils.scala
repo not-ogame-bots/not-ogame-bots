3baa02cd2241e14c920c919c89302a5130f86b46
ghostbuster91 <kghost0@gmail.com>
2020-05-17 01:14:16 +0200

    Rewrite waitForElement

--- selenium/src/main/scala/not/ogame/bots/selenium/WebDriverUtils.scala
+++ selenium/src/main/scala/not/ogame/bots/selenium/WebDriverUtils.scala
@@ -1,65 +1,47 @@
 package not.ogame.bots.selenium
 
-import cats.effect.IO
+import cats.effect.{IO, Timer}
 import org.openqa.selenium.{By, WebDriver, WebElement}
 
 import scala.jdk.CollectionConverters._
+import scala.concurrent.duration._
+import cats.implicits._
 
 object WebDriverUtils {
 
   implicit class RichWebElement(webElement: WebElement) {
+    def find(by: By): IO[WebElement] = IO.delay(webElement.findElement(by))
 
-    def find(by: By): IO[Option[WebElement]] =
-      IO.delay(Option(webElement.findElement(by)))
+    def findMany(by: By): IO[List[WebElement]] = IO.delay(webElement.findElements(by).asScala.toList)
 
-    def findMany(by: By): IO[List[WebElement]] =
-      IO.delay(webElement.findElements(by).asScala.toList)
+    def typeText(keys: String): IO[Unit] = IO.delay(webElement.sendKeys(keys))
 
-    def typeText(keys: String): IO[Unit] = {
-      IO.delay(webElement.sendKeys(keys))
-    }
-
-    def clickF(): IO[Unit] = {
-      IO.delay(webElement.click())
-    }
+    def clickF(): IO[Unit] = IO.delay(webElement.click())
   }
 }
 
 object WebDriverSyntax {
 
-  //TODO
-  def waitForElement(by: By)(implicit webDriver: WebDriver): IO[WebElement] =
-    IO.delay {
-      val startTime = System.currentTimeMillis()
-      while (webDriver.findElements(by).isEmpty) {
-        if (System.currentTimeMillis() - startTime > 10_000) {
-          throw new RuntimeException("Timeout waiting for element to become available: $by")
-        }
-        Thread.sleep(10)
-      }
-      webDriver.findElement(by)
-    }
+  def waitForElement(by: By, attempts: Int = 100)(implicit webDriver: WebDriver, timer: Timer[IO]): IO[WebElement] =
+    waitForElements(by, attempts).map(_.head)
 
-  //TODO
-  def waitForElements(by: By)(implicit webDriver: WebDriver): IO[List[WebElement]] = IO.delay {
-    val startTime = System.currentTimeMillis()
-    while (webDriver.findElements(by).isEmpty) {
-      if (System.currentTimeMillis() - startTime > 10_000) {
-        throw new RuntimeException("Timeout waiting for element to become available: $by")
-      }
-      Thread.sleep(10)
+  def waitForElements(by: By, attempts: Int = 100)(implicit webDriver: WebDriver, timer: Timer[IO]): IO[List[WebElement]] =
+    findMany(by).flatMap {
+      case l @ _ :: _          => IO.pure(l)
+      case Nil if attempts > 0 => IO.sleep(100 millis) >> waitForElements(by, attempts - 1)
+      case _                   => IO.raiseError(new RuntimeException(s"Timeout waiting for element to become available: $by"))
     }
-    webDriver.findElements(by).asScala.toList
-  }
 
   def switchToOtherTab()(implicit webDriver: WebDriver): IO[Unit] = IO.delay {
-    val otherHandle = webDriver.getWindowHandles.asScala.find(_ != webDriver.getWindowHandle).get
-    webDriver.switchTo().window(otherHandle)
+    val otherHandle = webDriver.getWindowHandles.asScala.find(_ != webDriver.getWindowHandle)
+    otherHandle.map(webDriver.switchTo().window(_))
   }
 
   def switchToAnyOpenTab()(implicit webDriver: WebDriver): IO[Unit] = IO.delay {
-    val firstHandle = webDriver.getWindowHandles.asScala.head
-    webDriver.switchTo().window(firstHandle)
+    webDriver.getWindowHandles.asScala.toList match {
+      case ::(head, _) => webDriver.switchTo().window(head)
+      case Nil         =>
+    }
   }
 
   def safeUrl(url: String)(implicit webDriver: WebDriver): IO[Unit] = {
@@ -72,16 +54,13 @@ object WebDriverSyntax {
     }
   }
 
-  def find(by: By)(implicit webDriver: WebDriver): IO[Option[WebElement]] =
-    IO.delay(Option(webDriver.findElement(by)))
+  def find(by: By)(implicit webDriver: WebDriver): IO[WebElement] =
+    IO.delay(webDriver.findElement(by))
 
   def findMany(by: By)(implicit webDriver: WebDriver): IO[List[WebElement]] =
     IO.delay(webDriver.findElements(by).asScala.toList)
 
   object go {
-
-    def to(url: String)(implicit driver: WebDriver): IO[Unit] = {
-      IO.delay(driver.get(url))
-    }
+    def to(url: String)(implicit driver: WebDriver): IO[Unit] = IO.delay(driver.get(url))
   }
 }
