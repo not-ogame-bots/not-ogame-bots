ff165663af7aaf35e8d40f9b16682448e4227d2b
Karol Kowalski <ordon1313@gmail.com>
2020-05-17 18:08:51 +0200

    Implement fetching supplies levels

--- selenium/src/main/scala/not/ogame/bots/selenium/Main.scala
+++ selenium/src/main/scala/not/ogame/bots/selenium/Main.scala
@@ -1,15 +1,15 @@
 package not.ogame.bots.selenium
 
 import cats.effect.{ExitCode, IO, IOApp}
-import not.ogame.bots.Credentials
 import cats.implicits._
+import not.ogame.bots.Credentials
 
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
     System.setProperty("webdriver.gecko.driver", "selenium/geckodriver")
     new SeleniumOgameDriverCreator()
       .create(Credentials("fire@fire.pl", "1qaz2wsx", "Mensa", "s165-pl"))
-      .use(ogame => ogame.login() >> IO.never)
+      .use(ogame => ogame.login() >> ogame.getSuppliesLevels("33794124").map(println(_)) >> IO.never)
       .as(ExitCode.Success)
   }
 }
