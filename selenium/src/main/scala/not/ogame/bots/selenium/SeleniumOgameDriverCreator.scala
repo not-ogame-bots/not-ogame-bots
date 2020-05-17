4d8b8ca8f468dfba5ed3a6b1aa52099376730a18
Karol Kowalski <ordon1313@gmail.com>
2020-05-17 13:35:45 +0200

    Crate new webDriver for each credentials

--- selenium/src/main/scala/not/ogame/bots/selenium/SeleniumOgameDriverCreator.scala
+++ selenium/src/main/scala/not/ogame/bots/selenium/SeleniumOgameDriverCreator.scala
@@ -2,11 +2,8 @@ package not.ogame.bots.selenium
 
 import cats.effect.{IO, Timer}
 import not.ogame.bots.{Credentials, OgameDriver, OgameDriverCreator}
-import org.openqa.selenium.WebDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 
 class SeleniumOgameDriverCreator(implicit timer: Timer[IO]) extends OgameDriverCreator[IO] {
-  private implicit val driver: WebDriver = new FirefoxDriver()
-
-  override def create(credentials: Credentials): OgameDriver[IO] = new SeleniumOgameDriver(credentials)
+  override def create(credentials: Credentials): OgameDriver[IO] = new SeleniumOgameDriver(credentials)(new FirefoxDriver(), timer)
 }
