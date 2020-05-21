c4cc3b53b8ae71a025198b8f08f7f5b5c87b7ee0
Karol Kowalski <ordon1313@gmail.com>
2020-05-21 17:57:54 +0200

    Add saved ogame page to resources Add /planet_list_component_reader/planets.html

---
+++ selenium/src/test/resources/planet_list_component_reader/planets_files/al.php
@@ -0,0 +1,3 @@
+var MAX_d6e42546 = '';
+MAX_d6e42546 += "<"+"div id=\'beacon_b69aa4cf3c\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'https://ads-delivery.gameforge.com/lg.php?bannerid=0&amp;campaignid=0&amp;zoneid=1331&amp;source=Quelle&amp;loc=https%3A%2F%2Fs168-pl.ogame.gameforge.com%2Fgame%2Findex.php%3Fpage%3Dingame%26component%3Doverview&amp;cb=b69aa4cf3c\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
+document.write(MAX_d6e42546);
