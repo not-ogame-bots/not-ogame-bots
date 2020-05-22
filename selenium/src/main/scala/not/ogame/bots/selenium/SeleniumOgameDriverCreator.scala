5850e1f11cd062c2cce867931f8c9d138e11a9cc
ghostbuster91 <kghost0@gmail.com>
2020-05-22 11:57:49 +0200

    Replace localDateTime with instant

--- selenium/src/main/scala/not/ogame/bots/selenium/SeleniumOgameDriverCreator.scala
+++ selenium/src/main/scala/not/ogame/bots/selenium/SeleniumOgameDriverCreator.scala
@@ -1,15 +1,17 @@
 package not.ogame.bots.selenium
 
+import java.time.Clock
+
 import cats.effect.{IO, Resource, Timer}
 import not.ogame.bots.{Credentials, OgameDriver, OgameDriverCreator}
 import org.openqa.selenium.firefox.FirefoxDriver
 
-class SeleniumOgameDriverCreator(implicit timer: Timer[IO]) extends OgameDriverCreator[IO] {
+class SeleniumOgameDriverCreator(implicit timer: Timer[IO], clock: Clock) extends OgameDriverCreator[IO] {
   override def create(credentials: Credentials): Resource[IO, OgameDriver[IO]] = {
     Resource
       .make(IO.delay(new FirefoxDriver()))(r => IO.delay(r.close()))
-      .map { driver =>
-        new SeleniumOgameDriver(credentials)(driver, timer)
+      .map { implicit driver =>
+        new SeleniumOgameDriver(credentials)
       }
   }
 }
