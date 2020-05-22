29efd20381f9664d6cc81eac73ba78249e887f22
Karol Kowalski <ordon1313@gmail.com>
2020-05-22 14:19:48 +0200

    Add non IO selenium helpers

---
+++ selenium/src/main/scala/not/ogame/bots/selenium/EasySelenium.scala
@@ -0,0 +1,54 @@
+package not.ogame.bots.selenium
+
+import org.openqa.selenium.{By, WebDriver, WebElement}
+
+import scala.jdk.CollectionConverters._
+
+object EasySelenium {
+  implicit class EasyWebDriver(webDriver: WebDriver) {
+    def safeUrl(url: String, attempts: Int = 3): Unit = {
+      webDriver.get(url)
+      if (webDriver.getCurrentUrl != url) {
+        if (attempts > 0) {
+          safeUrl(url, attempts - 1)
+        } else {
+          throw new RuntimeException(s"Couldn't proceed to page $url")
+        }
+      }
+    }
+
+    def waitForElement(by: By, attempts: Int = 100): Unit = {
+      val elements = webDriver.findElements(by)
+      if (elements.isEmpty) {
+        if (attempts > 0) {
+          Thread.sleep(100)
+          waitForElement(by, attempts - 1)
+        } else {
+          throw new RuntimeException(s"Time out waiting for $by")
+        }
+      }
+    }
+
+    def findElementsS(by: By): List[WebElement] = {
+      webDriver.findElements(by).asScala.toList
+    }
+  }
+
+  implicit class EasyWebElement(webElement: WebElement) {
+    def waitForElement(by: By, attempts: Int = 100): Unit = {
+      val elements = webElement.findElements(by)
+      if (elements.isEmpty) {
+        if (attempts > 0) {
+          Thread.sleep(100)
+          waitForElement(by, attempts - 1)
+        } else {
+          throw new RuntimeException(s"Time out waiting for $by")
+        }
+      }
+    }
+
+    def findElementsS(by: By): List[WebElement] = {
+      webElement.findElements(by).asScala.toList
+    }
+  }
+}
