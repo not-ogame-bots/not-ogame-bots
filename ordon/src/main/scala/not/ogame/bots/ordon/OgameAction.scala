0edc4dbdd3a77b782ce04f91f717d360e9187279
Karol Kowalski <ordon1313@gmail.com>
2020-05-23 16:53:51 +0200

    [Ordon] Remove monad reference from ScheduledAction

--- ordon/src/main/scala/not/ogame/bots/ordon/OgameAction.scala
+++ ordon/src/main/scala/not/ogame/bots/ordon/OgameAction.scala
@@ -2,18 +2,9 @@ package not.ogame.bots.ordon
 
 import java.time.LocalDateTime
 
-import cats.Monad
 import not.ogame.bots.OgameDriver
 
-case class ScheduledAction[T[_]: Monad](resumeOn: LocalDateTime, action: OgameAction[T]) {
-  def process(ogame: OgameDriver[T], now: LocalDateTime): T[List[ScheduledAction[T]]] = {
-    if (resumeOn.isBefore(now)) {
-      action.process(ogame)
-    } else {
-      Monad[T].pure(List(this))
-    }
-  }
-}
+case class ScheduledAction[T[_]](resumeOn: LocalDateTime, action: OgameAction[T])
 
 trait OgameAction[T[_]] {
   def process(ogame: OgameDriver[T]): T[List[ScheduledAction[T]]]
--- ordon/src/main/scala/not/ogame/bots/ordon/OrdonMain.scala
+++ ordon/src/main/scala/not/ogame/bots/ordon/OrdonMain.scala
@@ -34,9 +34,17 @@ object OrdonMain extends IOApp {
   private def process(ogame: OgameDriver[IO], scheduled: IO[List[ScheduledAction[IO]]]): IO[Unit] = {
     println("Process")
     val scheduledActionList = scheduled.unsafeRunSync()
-    val processed: IO[List[ScheduledAction[IO]]] = scheduledActionList.map(_.process(ogame, LocalDateTime.now())).sequence.map(_.flatten)
+    val processed: IO[List[ScheduledAction[IO]]] = scheduledActionList.map(process(_, ogame, LocalDateTime.now())).sequence.map(_.flatten)
     val withSleep = processed
       .flatMap(a => IO.sleep(1 second).map(_ => a))
     process(ogame, withSleep)
+  }
+
+  private def process(scheduledAction: ScheduledAction[IO], ogame: OgameDriver[IO], now: LocalDateTime): IO[List[ScheduledAction[IO]]] = {
+    if (scheduledAction.resumeOn.isBefore(now)) {
+      scheduledAction.action.process(ogame)
+    } else {
+      IO.pure(List(scheduledAction))
+    }
   }
 }
