9ddcd0a00efd9b6885f9a96ff663ca8f2567c4a1
ghostbuster91 <kghost0@gmail.com>
2020-05-23 20:00:43 +0200

    [GBot] Refresh buildings on fleet arrival

--- ghostbuster/src/main/scala/not/ogame/bots/ghostbuster/processors/FlyAroundProcessor.scala
+++ ghostbuster/src/main/scala/not/ogame/bots/ghostbuster/processors/FlyAroundProcessor.scala
@@ -1,22 +1,36 @@
 package not.ogame.bots.ghostbuster.processors
 
-import java.time.ZoneOffset
-
-import not.ogame.bots.{Fleet, FleetAttitude, FleetMissionType, FleetResources, SendFleetRequest, SendFleetRequestShips}
+import not.ogame.bots.{CoordinatesType, Fleet, FleetAttitude, FleetMissionType, FleetResources, SendFleetRequest, SendFleetRequestShips}
 import not.ogame.bots.ghostbuster.{PlanetState, RandomTimeJitter, State, Task}
 import com.softwaremill.quicklens._
 
 class FlyAroundProcessor(jitter: RandomTimeJitter) {
   def apply(state: State.LoggedIn): State.LoggedIn = {
     state.fleets
-      .find(_.fleetAttitude == FleetAttitude.Friendly)
+      .find(
+        f =>
+          f.fleetAttitude == FleetAttitude.Friendly && f.to.coordinatesType == CoordinatesType.Planet && f.fleetMissionType == FleetMissionType.Deployment
+      )
       .map { myFleet =>
         state.modify(_.scheduledTasks).using(_ ++ singleFleet(myFleet, state.scheduledTasks, state.planets))
       }
       .getOrElse(state)
   }
 
-  def singleFleet(fleet: Fleet, tasks: List[Task], planets: List[PlanetState]): List[Task] = {
+  private def singleFleet(fleet: Fleet, tasks: List[Task], planets: List[PlanetState]): List[Task] = {
+    scheduleFleetReverse(fleet, tasks, planets) ++ scheduleRefreshBuildings(fleet, tasks, planets)
+  }
+
+  private def scheduleRefreshBuildings(fleet: Fleet, tasks: List[Task], planets: List[PlanetState]): List[Task] = {
+    val toPlanetId = planets.find(p => p.coords == fleet.to).get.id
+    if (!checkAlreadyInQueue(tasks) { case t: Task.RefreshSupplyAndFacilityPage if t.planetId == toPlanetId => t }) {
+      List(Task.RefreshSupplyAndFacilityPage(fleet.arrivalTime.plusSeconds(1), toPlanetId))
+    } else {
+      List.empty
+    }
+  }
+
+  private def scheduleFleetReverse(fleet: Fleet, tasks: List[Task], planets: List[PlanetState]) = {
     if (!checkAlreadyInQueue(tasks) { case t: Task.SendFleet => t }) {
       val fromPlanetId = planets.find(p => p.coords == fleet.to).get
       List(
