de2b27f911acf42151872c0b6826bbaba2953484
Karol Kowalski <ordon1313@gmail.com>
2020-05-24 23:41:39 +0200

    [Ordon] Add FlyAroundOgameAction

--- ordon/src/main/scala/not/ogame/bots/ordon/OgameAction.scala
+++ ordon/src/main/scala/not/ogame/bots/ordon/OgameAction.scala
@@ -2,10 +2,19 @@ package not.ogame.bots.ordon
 
 import java.time.Instant
 
+import cats.Monad
+import cats.implicits._
 import not.ogame.bots.OgameDriver
 
 case class ScheduledAction[T[_]](resumeOn: Instant, action: OgameAction[T])
 
 trait OgameAction[T[_]] {
   def process(ogame: OgameDriver[T]): T[List[ScheduledAction[T]]]
+}
+
+abstract class SimpleOgameAction[T[_]: Monad] extends OgameAction[T] {
+  def processSimple(ogame: OgameDriver[T]): T[Instant]
+
+  final def process(ogame: OgameDriver[T]): T[List[ScheduledAction[T]]] =
+    processSimple(ogame).map(resumeOn => List(ScheduledAction(resumeOn, this)))
 }
--- ordon/src/main/scala/not/ogame/bots/ordon/OrdonConfig.scala
+++ ordon/src/main/scala/not/ogame/bots/ordon/OrdonConfig.scala
@@ -3,8 +3,9 @@ package not.ogame.bots.ordon
 import java.time.Instant
 
 import cats.effect.IO
+import cats.implicits._
 import not.ogame.bots.ShipType.{Destroyer, EspionageProbe, Explorer, LargeCargoShip}
-import not.ogame.bots.{Coordinates, Credentials}
+import not.ogame.bots.{Coordinates, CoordinatesType, Credentials, PlayerPlanet}
 
 object OrdonConfig {
   def getCredentials: Credentials = {
@@ -18,7 +19,7 @@ object OrdonConfig {
   }
 
   def getInitialActions: IO[List[ScheduledAction[IO]]] = {
-    val listOfActions = List(createExpeditionAction)
+    val listOfActions = List(createExpeditionAction, createFlyAroundActionCargo, createFlyAroundActionBattle)
     IO.pure(listOfActions.map(ScheduledAction(Instant.now(), _)))
   }
 
@@ -30,4 +31,36 @@ object OrdonConfig {
       targetCoordinates = Coordinates(3, 133, 16)
     )
   }
+
+  private def createFlyAroundActionCargo: FlyAroundOgameAction[IO] = {
+    new FlyAroundOgameAction[IO](
+      targets = List(moon4, moon5),
+      fleetSelector = new FleetSelector[IO](
+        filters = Map(
+          Destroyer -> Selector.skip,
+          EspionageProbe -> Selector.skip,
+          Explorer -> Selector.skip,
+          LargeCargoShip -> Selector.decreaseBy(300)
+        )
+      ),
+      resourceSelector = new ResourceSelector[IO](deuteriumSelector = Selector.decreaseBy(200_000))
+    )
+  }
+
+  private def createFlyAroundActionBattle: FlyAroundOgameAction[IO] = {
+    new FlyAroundOgameAction[IO](
+      targets = List(moon4, moon5),
+      fleetSelector = new FleetSelector[IO](),
+      resourceSelector = new ResourceSelector[IO]()
+    )
+  }
+
+  private val planet4 = PlayerPlanet("33638312", Coordinates(3, 133, 4))
+  private val planet5 = PlayerPlanet("33642425", Coordinates(3, 133, 5))
+  private val planet6 = PlayerPlanet("33641548", Coordinates(3, 133, 6))
+  private val planet7 = PlayerPlanet("33639665", Coordinates(3, 133, 7))
+  private val moon4 = PlayerPlanet("33645637", Coordinates(3, 133, 4, CoordinatesType.Moon))
+  private val moon5 = PlayerPlanet("33645302", Coordinates(3, 133, 5, CoordinatesType.Moon))
+  private val moon6 = PlayerPlanet("33645566", Coordinates(3, 133, 6, CoordinatesType.Moon))
+  private val moon7 = PlayerPlanet("33657126", Coordinates(3, 133, 7, CoordinatesType.Moon))
 }
