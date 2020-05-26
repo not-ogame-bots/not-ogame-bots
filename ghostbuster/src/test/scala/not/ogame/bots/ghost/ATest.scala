916edd60b4f01e8bd051c55b6edea4f4eb5dbf9f
ghostbuster91 <kghost0@gmail.com>
2020-05-26 19:08:35 +0200

    Rewrite date test to use zdt

--- ghostbuster/src/test/scala/not/ogame/bots/ghost/ATest.scala
+++ ghostbuster/src/test/scala/not/ogame/bots/ghost/ATest.scala
@@ -1,18 +1,19 @@
 package not.ogame.bots.ghost
 
-import java.time.{Clock, Instant, LocalDateTime, ZoneOffset, ZonedDateTime}
+import java.time.{Clock, Instant, ZonedDateTime, ZoneOffset}
 
 class ATest extends munit.FunSuite {
   val clock1 = Clock.system(ZoneOffset.ofHours(3))
 
   test("date") {
-    val ldt = LocalDateTime.now(clock1)
-    println(s"bare ldt ${LocalDateTime.now()}")
+    val ldt = ZonedDateTime.now(clock1)
+    val ldtNow = ZonedDateTime.now()
+    println(s"bare ldt $ldtNow")
     println(s"clock ldt $ldt")
     println(s"bare instant ${Instant.now()}")
     println(s"clock instant ${clock1.instant()}")
-    println(s"ldt to instant using clock ${ldt.toInstant(ZoneOffset.of(clock1.getZone.getId))}")
-    println(s"ldt to insstant w/o clock ${ldt.toInstant(ZoneOffset.ofHours(0))}")
+    println(s"ldt to instant using clock ${ldt.toInstant}")
+    println(s"ldt to insstant w/o clock ${ldtNow.toInstant}")
 
     val zdt = ZonedDateTime.now(clock1)
     println(s"bare ldt ${ZonedDateTime.now()}")
