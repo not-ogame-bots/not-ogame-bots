fbbccc3e82550cdf1c6cf6e2250b1a53c459363d
ghostbuster91 <kghost0@gmail.com>
2020-05-27 12:25:26 +0200

    Switch to MVar and simplify code

--- ghostbuster/src/main/scala/not/ogame/bots/ghostbuster/executor/Action.scala
+++ ghostbuster/src/main/scala/not/ogame/bots/ghostbuster/executor/Action.scala
@@ -1,60 +1,47 @@
 package not.ogame.bots.ghostbuster.executor
 
 import java.time.ZonedDateTime
-import java.util.UUID
 
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.numeric.Positive
-import not.ogame.bots.ghostbuster.PlanetFleet
 import not.ogame.bots._
+import not.ogame.bots.ghostbuster.PlanetFleet
 
 sealed trait Action[T] {
-  def uuid: UUID
-  def executionTime: ZonedDateTime
   def defer(any: Any): T = any.asInstanceOf[T]
-  def success(value: T): Response = Response.Success(value, uuid)
-  def failure(): Response = Response.Failure(uuid)
+  def success(value: T): Response = Response.Success(value)
+  def failure(throwable: Throwable): Response = Response.Failure(throwable)
 }
 
-sealed trait Response {
-  def uuid: UUID
-}
+sealed trait Response
 object Response {
-  case class Success(value: Any, uuid: UUID) extends Response
-  case class Failure(uuid: UUID) extends Response
+  case class Success(value: Any) extends Response
+  case class Failure(ex: Throwable) extends Response
 }
 
 object Action {
   case class BuildSupply(
       suppliesBuilding: SuppliesBuilding,
       level: Int Refined Positive,
-      executionTime: ZonedDateTime,
-      planetId: String,
-      uuid: UUID = UUID.randomUUID()
+      planetId: String
   ) extends Action[ZonedDateTime]
 
   case class BuildFacility(
       facilityBuilding: FacilityBuilding,
       level: Int Refined Positive,
-      executionTime: ZonedDateTime,
-      planetId: String,
-      uuid: UUID = UUID.randomUUID()
+      planetId: String
   ) extends Action[ZonedDateTime]
 
-  case class ReadSupplyPage(executionTime: ZonedDateTime, planetId: String, uuid: UUID = UUID.randomUUID()) extends Action[SuppliesPageData]
-  case class ReadFacilityPage(executionTime: ZonedDateTime, planetId: String, uuid: UUID = UUID.randomUUID())
-      extends Action[FacilityPageData]
+  case class ReadSupplyPage(planetId: String) extends Action[SuppliesPageData]
+  case class ReadFacilityPage(planetId: String) extends Action[FacilityPageData]
 
-  case class RefreshFleetOnPlanetStatus(executionTime: ZonedDateTime, playerPlanet: PlayerPlanet, uuid: UUID = UUID.randomUUID())
-      extends Action[PlanetFleet]
+  case class RefreshFleetOnPlanetStatus(playerPlanet: PlayerPlanet) extends Action[PlanetFleet]
 
-  case class BuildShip(amount: Int, shipType: ShipType, executionTime: ZonedDateTime, planetId: String, uuid: UUID = UUID.randomUUID())
-      extends Action[SuppliesPageData]
+  case class BuildShip(amount: Int, shipType: ShipType, planetId: String) extends Action[SuppliesPageData]
 
-  case class SendFleet(executionTime: ZonedDateTime, sendFleetRequest: SendFleetRequest, uuid: UUID = UUID.randomUUID())
-      extends Action[ZonedDateTime]
+  case class SendFleet(sendFleetRequest: SendFleetRequest) extends Action[ZonedDateTime]
 
-  case class GetAirFleet(executionTime: ZonedDateTime, uuid: UUID = UUID.randomUUID()) extends Action[List[Fleet]]
+  case class GetAirFleet() extends Action[List[Fleet]]
 
-  case class ReadPlanets(executionTime: ZonedDateTime, uuid: UUID = UUID.randomUUID()) extends Action[List[PlayerPlanet]]
+  case class ReadPlanets() extends Action[List[PlayerPlanet]]
 }
--- ghostbuster/src/main/scala/not/ogame/bots/ghostbuster/executor/TaskExecutorImpl.scala
+++ ghostbuster/src/main/scala/not/ogame/bots/ghostbuster/executor/TaskExecutorImpl.scala
@@ -2,17 +2,14 @@ package not.ogame.bots.ghostbuster.executor
 
 import java.time.ZonedDateTime
 
+import cats.effect.concurrent.MVar
 import cats.implicits._
 import com.typesafe.scalalogging.StrictLogging
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.numeric.Positive
 import io.chrisdavenport.log4cats.Logger
-import monix.catnap.ConcurrentQueue
 import monix.eval.Task
-import monix.execution.BufferCapacity
 import monix.execution.Scheduler.Implicits.global
-import monix.reactive.subjects.ConcurrentSubject
-import monix.reactive.{Consumer, MulticastStrategy}
 import not.ogame.bots._
 import not.ogame.bots.ghostbuster.processors.TaskExecutor
 import not.ogame.bots.ghostbuster.{FLogger, PlanetFleet}
@@ -21,8 +18,10 @@ import scala.concurrent.duration._
 import scala.jdk.DurationConverters._
 
 class TaskExecutorImpl(ogameDriver: OgameDriver[Task], clock: LocalClock) extends TaskExecutor with FLogger with StrictLogging {
-  private val subject = ConcurrentSubject(MulticastStrategy.publish[Response])
-  private val queue = ConcurrentQueue[Task].unsafe[Action[_]](BufferCapacity.Unbounded())
+  type Channel[A] = MVar[Task, A]
+
+  private val responses = MVar[Task].empty[Response].runSyncUnsafe()
+  private val requests = MVar[Task].empty[Action[_]].runSyncUnsafe()
 
   def run(): Task[Unit] = {
     safeLogin() >> processNextAction()
@@ -30,7 +29,7 @@ class TaskExecutorImpl(ogameDriver: OgameDriver[Task], clock: LocalClock) extend
 
   private def processNextAction(): Task[Unit] = {
     for {
-      action <- queue.poll
+      action <- requests.take
       _ <- Logger[Task].debug(s"executing action: ${pprint.apply(action)}")
       _ <- safeHandleAction(action)
       _ <- processNextAction()
@@ -39,14 +38,14 @@ class TaskExecutorImpl(ogameDriver: OgameDriver[Task], clock: LocalClock) extend
 
   private def safeHandleAction(action: Action[_]): Task[Unit] = {
     handleAction(action)
-      .flatMap(response => Task.fromFuture(subject.onNext(response)).void)
+      .flatMap(response => responses.put(response))
       .handleErrorWith { e =>
         for {
           _ <- Logger[Task].error(e)(e.getMessage)
           isStillLogged <- ogameDriver.checkIsLoggedIn()
           _ <- if (isStillLogged) {
             Logger[Task].warn("still logged, failing action...") >>
-              Task.fromFuture(subject.onNext(action.failure())) >> processNextAction()
+              responses.put(action.failure(e)) >> processNextAction()
           } else {
             Logger[Task].warn("not logged") >>
               safeLogin >> safeHandleAction(action)
@@ -66,34 +65,34 @@ class TaskExecutorImpl(ogameDriver: OgameDriver[Task], clock: LocalClock) extend
 
   private def handleAction(action: Action[_]) = {
     action match {
-      case a @ Action.BuildSupply(suppliesBuilding, _, _, planetId, _) =>
+      case a @ Action.BuildSupply(suppliesBuilding, _, planetId) =>
         ogameDriver
           .buildSuppliesBuilding(planetId, suppliesBuilding)
           .flatMap(_ => ogameDriver.readSuppliesPage(planetId).map(_.currentBuildingProgress.get))
           .map(buildingProgress => a.success(buildingProgress.finishTimestamp))
-      case a @ Action.BuildFacility(facilityBuilding, _, _, planetId, _) =>
+      case a @ Action.BuildFacility(facilityBuilding, _, planetId) =>
         ogameDriver
           .buildFacilityBuilding(planetId, facilityBuilding)
           .flatMap(_ => ogameDriver.readFacilityPage(planetId).map(_.currentBuildingProgress.get))
           .map(buildingProgress => a.success(buildingProgress.finishTimestamp))
-      case a @ Action.ReadSupplyPage(_, planetId, _) =>
+      case a @ Action.ReadSupplyPage(planetId) =>
         ogameDriver
           .readSuppliesPage(planetId)
           .map(sp => a.success(sp))
-      case a @ Action.ReadFacilityPage(_, planetId, _) =>
+      case a @ Action.ReadFacilityPage(planetId) =>
         ogameDriver
           .readFacilityPage(planetId)
           .map(fp => a.success(fp))
-      case a @ Action.RefreshFleetOnPlanetStatus(_, planetId, _) =>
+      case a @ Action.RefreshFleetOnPlanetStatus(planetId) =>
         ogameDriver
           .checkFleetOnPlanet(planetId.id)
           .map(f => a.success(PlanetFleet(planetId, f)))
-      case a @ Action.BuildShip(amount, shipType, _, planetId, _) =>
+      case a @ Action.BuildShip(amount, shipType, planetId) =>
         ogameDriver
           .buildShips(planetId, shipType, amount)
           .flatMap(_ => ogameDriver.readSuppliesPage(planetId))
           .map(sp => a.success(sp))
-      case a @ Action.SendFleet(_, sendFleetRequest, _) =>
+      case a @ Action.SendFleet(sendFleetRequest) =>
         ogameDriver.sendFleet(sendFleetRequest).flatMap { _ =>
           ogameDriver
             .readAllFleets()
@@ -105,12 +104,12 @@ class TaskExecutorImpl(ogameDriver: OgameDriver[Task], clock: LocalClock) extend
             .flatTap(_ => ogameDriver.readPlanets())
             .map(f => a.success(f.arrivalTime))
         }
-      case a @ Action.GetAirFleet(_, _) =>
+      case a: Action.GetAirFleet =>
         ogameDriver
           .readAllFleets()
           .flatTap(_ => ogameDriver.readPlanets())
           .map(fleets => a.success(fleets))
-      case a @ Action.ReadPlanets(_, _) =>
+      case a: Action.ReadPlanets =>
         ogameDriver
           .readPlanets()
           .map(planets => a.success(planets))
@@ -134,32 +133,32 @@ class TaskExecutorImpl(ogameDriver: OgameDriver[Task], clock: LocalClock) extend
   }
 
   override def readAllFleets(): Task[List[Fleet]] = {
-    val action = Action.GetAirFleet(clock.now())
+    val action = Action.GetAirFleet()
     exec(action)
   }
 
   override def readPlanets(): Task[List[PlayerPlanet]] = {
-    val action = Action.ReadPlanets(clock.now())
+    val action = Action.ReadPlanets()
     exec(action)
   }
 
   override def sendFleet(req: SendFleetRequest): Task[ZonedDateTime] = {
-    val action = Action.SendFleet(clock.now(), req)
+    val action = Action.SendFleet(req)
     exec(action)
   }
 
   override def getFleetOnPlanet(planet: PlayerPlanet): Task[PlanetFleet] = {
-    val action = Action.RefreshFleetOnPlanetStatus(clock.now(), planet)
+    val action = Action.RefreshFleetOnPlanetStatus(planet)
     exec(action)
   }
 
   override def readSupplyPage(playerPlanet: PlayerPlanet): Task[SuppliesPageData] = {
-    val action = Action.ReadSupplyPage(clock.now(), playerPlanet.id)
+    val action = Action.ReadSupplyPage(playerPlanet.id)
     exec(action)
   }
 
   override def readFacilityPage(playerPlanet: PlayerPlanet): Task[FacilityPageData] = {
-    val action = Action.ReadFacilityPage(clock.now(), playerPlanet.id)
+    val action = Action.ReadFacilityPage(playerPlanet.id)
     exec(action)
   }
 
@@ -168,7 +167,7 @@ class TaskExecutorImpl(ogameDriver: OgameDriver[Task], clock: LocalClock) extend
       level: Int Refined Positive,
       planet: PlayerPlanet
   ): Task[ZonedDateTime] = {
-    val action = Action.BuildSupply(suppliesBuilding, level, clock.now(), planet.id)
+    val action = Action.BuildSupply(suppliesBuilding, level, planet.id)
     exec(action)
   }
 
@@ -177,29 +176,24 @@ class TaskExecutorImpl(ogameDriver: OgameDriver[Task], clock: LocalClock) extend
       level: Refined[Int, Positive],
       planet: PlayerPlanet
   ): Task[ZonedDateTime] = {
-    val action = Action.BuildFacility(facilityBuilding, level, clock.now(), planet.id)
+    val action = Action.BuildFacility(facilityBuilding, level, planet.id)
     exec(action)
   }
 
   override def buildShip(shipType: ShipType, amount: Int, head: PlayerPlanet): Task[SuppliesPageData] = {
-    val action = Action.BuildShip(amount, shipType, clock.now(), head.id)
+    val action = Action.BuildShip(amount, shipType, head.id)
     exec(action)
   }
 
   private def exec[T](action: Action[T]) = {
-    Task
-      .parMap2(
-        queue.offer(action),
-        subject
-          .filter(r => r.uuid == action.uuid)
-          .consumeWith(Consumer.head)
-      )((_, result) => result)
-      .flatMap {
-        case Response.Success(anyValue, _) =>
-          val value = action.defer(anyValue)
-          Logger[Task].debug(s"action response: ${pprint.apply(value)}").map(_ => value)
-        case Response.Failure(_) =>
-          Task.raiseError[T](new RuntimeException("Couldn't execute operation"))
-      }
+    requests.put(action) >>
+      responses.take
+        .flatMap {
+          case Response.Success(anyValue) =>
+            val value = action.defer(anyValue)
+            Logger[Task].debug(s"action response: ${pprint.apply(value)}").map(_ => value)
+          case Response.Failure(_) =>
+            Task.raiseError[T](new RuntimeException("Couldn't execute operation"))
+        }
   }
 }
