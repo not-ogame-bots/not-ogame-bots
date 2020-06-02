62aed969cb423592bb449b7d5f9047b9d17fd1cb
ghostbuster91 <kghost0@gmail.com>
2020-06-02 23:14:27 +0200

    Rewrite ActivityFakerProcessor to reactive style

--- ghostbuster/src/main/scala/not/ogame/bots/ghostbuster/processors/ActivityFakerProcessor.scala
+++ ghostbuster/src/main/scala/not/ogame/bots/ghostbuster/processors/ActivityFakerProcessor.scala
@@ -3,26 +3,32 @@ package not.ogame.bots.ghostbuster.processors
 import cats.implicits._
 import io.chrisdavenport.log4cats.Logger
 import monix.eval.Task
+import monix.reactive.{Consumer, Observable}
 import not.ogame.bots.ghostbuster.FLogger
 
 import scala.concurrent.duration._
 
 class ActivityFakerProcessor(taskExecutor: TaskExecutor) extends FLogger {
   def run(): Task[Unit] = {
-    Logger[Task].info("activity faker sleeping...") >> Task.sleep(14 minutes) >> checkSomething >> run()
+    taskExecutor.subscribeToNotifications
+      .switchMap { _ =>
+        Observable.intervalAtFixedRate(14 minutes, 14 minutes)
+      }
+      .consumeWith(Consumer.foreachTask(_ => checkSomething))
   }
 
   private def checkSomething = {
-    taskExecutor
-      .readPlanets()
-      .flatMap { planets =>
-        planets
-          .take(2)
-          .map { planet =>
-            taskExecutor.readSupplyPage(planet)
-          }
-          .sequence
-      }
-      .void
+    Logger[Task].info("activity faker running...") >>
+      taskExecutor
+        .readPlanets()
+        .flatMap { planets =>
+          planets
+            .take(2)
+            .map { planet =>
+              taskExecutor.readSupplyPage(planet)
+            }
+            .sequence
+        }
+        .void
   }
 }
