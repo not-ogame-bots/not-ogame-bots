14054e7c667cac7d455f1538252a32f310cd8944
Karol Kowalski <ordon1313@gmail.com>
2020-06-04 23:32:45 +0200

    [Selenium] Add saved galaxy page

---
+++ selenium/src/test/resources/galaxy_component_reader/galaxy_files/al.php
@@ -0,0 +1,3 @@
+var MAX_e913b8c0 = '';
+MAX_e913b8c0 += "<"+"div id=\'beacon_05c00053ca\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'https://ads-delivery.gameforge.com/lg.php?bannerid=0&amp;campaignid=0&amp;zoneid=1331&amp;source=Quelle&amp;loc=https%3A%2F%2Fs168-pl.ogame.gameforge.com%2Fgame%2Findex.php%3Fpage%3Dingame%26component%3Dgalaxy&amp;cb=05c00053ca\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
+document.write(MAX_e913b8c0);
