ee0ef9aa4c594d0891818cb7393e99161a2ae637
Karol Kowalski <ordon1313@gmail.com>
2020-06-07 14:12:52 +0200

    [Ordon] [Refactor] Extract oneAfterOther function for List[T[Unit]]

--- ordon/src/main/scala/not/ogame/bots/ordon/KeepActiveOgameAction.scala
+++ ordon/src/main/scala/not/ogame/bots/ordon/KeepActiveOgameAction.scala
@@ -11,10 +11,14 @@ import scala.util.Random
 class KeepActiveOgameAction[T[_]: Monad](planets: List[PlayerPlanet])(implicit clock: LocalClock) extends SimpleOgameAction[T] {
   override def processSimple(ogame: OgameDriver[T]): T[ZonedDateTime] = {
     val actions: List[T[Unit]] = Random.shuffle(planets).take(planets.size / 2 + 1).map(it => ogame.readSuppliesPage(it.id).map(a => ()))
-    val unitT: T[Unit] = Monad[T].pure(())
-    val joinedActions: T[Unit] = actions.fold(unitT) { (a: T[Unit], b: T[Unit]) =>
-      a.flatMap(c => b)
+    val joinedActions: T[Unit] = oneAfterOther(actions)
+    joinedActions.map(_ => clock.now().plusMinutes(13).plusSeconds(40))
+  }
+
+  private def oneAfterOther(actions: List[T[Unit]]): T[Unit] = {
+    val unitT: T[Unit] = ().pure[T]
+    actions.fold(unitT) { (a: T[Unit], b: T[Unit]) =>
+      a.flatMap(_ => b)
     }
-    joinedActions.map(a => clock.now().plusMinutes(13).plusSeconds(40))
   }
 }
