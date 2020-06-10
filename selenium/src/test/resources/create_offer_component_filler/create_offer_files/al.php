5698e408b813a59df50a79e8be924fb65515c05c
Karol Kowalski <ordon1313@gmail.com>
2020-06-10 21:39:34 +0200

    [Selenium] Add create_offer.html to test resources

---
+++ selenium/src/test/resources/create_offer_component_filler/create_offer_files/al.php
@@ -0,0 +1,3 @@
+var MAX_5ab8d02c = '';
+MAX_5ab8d02c += "<"+"div id=\'beacon_99141d5b47\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'https://ads-delivery.gameforge.com/lg.php?bannerid=0&amp;campaignid=0&amp;zoneid=1331&amp;source=Quelle&amp;loc=https%3A%2F%2Fs168-pl.ogame.gameforge.com%2Fgame%2Findex.php%3Fpage%3Dingame%26component%3Dmarketplace%26tab%3Dcreate_offer&amp;cb=99141d5b47\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
+document.write(MAX_5ab8d02c);
