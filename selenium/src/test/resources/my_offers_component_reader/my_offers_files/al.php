4ec0527d5039e43b4981064659bb72bf2c97c191
Karol Kowalski <ordon1313@gmail.com>
2020-06-10 18:28:36 +0200

    [Selenium] Add my_offers.html to test resources

---
+++ selenium/src/test/resources/my_offers_component_reader/my_offers_files/al.php
@@ -0,0 +1,3 @@
+var MAX_8b7eea00 = '';
+MAX_8b7eea00 += "<"+"div id=\'beacon_cab68c1958\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'https://ads-delivery.gameforge.com/lg.php?bannerid=0&amp;campaignid=0&amp;zoneid=1331&amp;source=Quelle&amp;loc=https%3A%2F%2Fs168-pl.ogame.gameforge.com%2Fgame%2Findex.php%3Fpage%3Dingame%26component%3Dmarketplace%26tab%3Doverview&amp;cb=cab68c1958\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
+document.write(MAX_8b7eea00);
