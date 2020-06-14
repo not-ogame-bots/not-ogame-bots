5c836aa4b0d17877ccc0e2e8071e09359812287e
Karol Kowalski <ordon1313@gmail.com>
2020-06-14 17:59:42 +0200

    [Selenium] Add send_fleet.html to test resources

---
+++ selenium/src/test/resources/send_fleet_action/send_fleet_files/al.php
@@ -0,0 +1,3 @@
+var MAX_c63ffa9a = '';
+MAX_c63ffa9a += "<"+"div id=\'beacon_d26f5141c0\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'https://ads-delivery.gameforge.com/lg.php?bannerid=0&amp;campaignid=0&amp;zoneid=1331&amp;source=Quelle&amp;loc=https%3A%2F%2Fs165-pl.ogame.gameforge.com%2F&amp;cb=d26f5141c0\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
+document.write(MAX_c63ffa9a);
