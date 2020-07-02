110910d6870714dd24403c78282452978a200ae3
Karol Kowalski <ordon1313@gmail.com>
2020-07-02 19:06:52 +0200

    [Selenium] Add galaxy_debris.html

---
+++ selenium/src/test/resources/galaxy_component_reader/galaxy_debris_files/al.php
@@ -0,0 +1,3 @@
+var MAX_a8a667a2 = '';
+MAX_a8a667a2 += "<"+"div id=\'beacon_ed0676fb90\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'https://ads-delivery.gameforge.com/lg.php?bannerid=0&amp;campaignid=0&amp;zoneid=1331&amp;source=Quelle&amp;loc=https%3A%2F%2Fs169-pl.ogame.gameforge.com%2Fgame%2Findex.php%3Fpage%3Dingame%26component%3Dgalaxy&amp;cb=ed0676fb90\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
+document.write(MAX_a8a667a2);
