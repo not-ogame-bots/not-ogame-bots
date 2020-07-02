da09dbbb767cb116453b975dcc169b8153c55505
Karol Kowalski <ordon1313@gmail.com>
2020-07-02 22:23:16 +0200

    [Ordon] Add ExpeditionCollectDebrisOrdonAction

---
+++ ordon/src/main/scala/not/ogame/bots/ordon/action/ExpeditionCollectDebrisOrdonAction.scala
@@ -0,0 +1,44 @@
+package not.ogame.bots.ordon.action
+
+import not.ogame.bots.CoordinatesType.Debris
+import not.ogame.bots.FleetMissionType.Recycle
+import not.ogame.bots.FleetSpeed.Percent100
+import not.ogame.bots.SendFleetRequestShips.Ships
+import not.ogame.bots.ShipType.Explorer
+import not.ogame.bots.ordon.core._
+import not.ogame.bots.ordon.utils.SendFleet
+import not.ogame.bots.{PlayerPlanet, Resources}
+
+class ExpeditionCollectDebrisOrdonAction(expeditionPlanet: PlayerPlanet) extends BaseOrdonAction {
+  override def shouldHandleEvent(event: OrdonEvent): Boolean = {
+    event.isInstanceOf[ExpeditionFleetChanged]
+  }
+
+  override def doProcess(event: OrdonEvent, ogame: OrdonOgameDriver, eventRegistry: EventRegistry): List[OrdonAction] = {
+    handleExpeditionChangeEvent(ogame, event.asInstanceOf[ExpeditionFleetChanged])
+    List(this)
+  }
+
+  private def handleExpeditionChangeEvent(ogame: OrdonOgameDriver, expeditionFleetChanged: ExpeditionFleetChanged): Unit = {
+    val expeditionDebrisTarget = expeditionFleetChanged.to.copy(coordinatesType = Debris)
+    val galaxyPageData = ogame.readGalaxyPage(expeditionPlanet.id, expeditionDebrisTarget.galaxy, expeditionDebrisTarget.system)
+    val myFleetPageData = ogame.readMyFleets()
+    val isAlreadyCollectingDebris = myFleetPageData.fleets.exists(
+      fleet => fleet.from == expeditionPlanet.coordinates && fleet.to == expeditionDebrisTarget && !fleet.isReturning
+    )
+    val isThereDebrisToCollect = galaxyPageData.debrisMap.contains(expeditionDebrisTarget)
+    if (!isAlreadyCollectingDebris && isThereDebrisToCollect) {
+      val sendFleetRequest = new SendFleet(
+        from = expeditionPlanet,
+        to = expeditionPlanet,
+        selectResources = _ => Resources.Zero,
+        selectShips = page => Map(Explorer -> Math.min(page.ships(Explorer), 100)),
+        fleetSpeed = Percent100,
+        missionType = Recycle
+      ).getSendFleetRequest(ogame)
+      if (sendFleetRequest.ships.asInstanceOf[Ships].ships.values.sum > 0) {
+        ogame.sendFleet(sendFleetRequest)
+      }
+    }
+  }
+}
