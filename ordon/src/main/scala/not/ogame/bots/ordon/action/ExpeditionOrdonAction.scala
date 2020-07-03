f164a44ebad434db38e014a98f1f32fca0596408
Karol Kowalski <ordon1313@gmail.com>
2020-07-03 19:47:08 +0200

    [Ordon] Balance expedition fleet

--- ordon/src/main/scala/not/ogame/bots/ordon/action/ExpeditionOrdonAction.scala
+++ ordon/src/main/scala/not/ogame/bots/ordon/action/ExpeditionOrdonAction.scala
@@ -4,12 +4,12 @@ import java.time.ZonedDateTime
 
 import not.ogame.bots.CoordinatesType.Planet
 import not.ogame.bots.FleetMissionType.Expedition
+import not.ogame.bots.ShipType._
 import not.ogame.bots._
 import not.ogame.bots.ordon.core.{EventRegistry, ExpeditionFleetChanged, OrdonOgameDriver, TimeBasedOrdonAction}
-import not.ogame.bots.ordon.utils.SelectShips
 
-class ExpeditionOrdonAction(val startPlanet: PlayerPlanet, val expeditionFleet: Map[ShipType, Int]) extends TimeBasedOrdonAction {
-  private val selectShips: SelectShips = page => page.ships.map(e => e._1 -> Math.min(e._2, expeditionFleet.getOrElse(e._1, 0)))
+class ExpeditionOrdonAction(val startPlanet: PlayerPlanet) extends TimeBasedOrdonAction {
+  private val expeditionFleetHelper = new ExpeditionFleetHelper()
 
   override def processTimeBased(ogame: OrdonOgameDriver, eventRegistry: EventRegistry): ZonedDateTime = {
     val page = ogame.readMyFleets()
@@ -30,11 +30,33 @@ class ExpeditionOrdonAction(val startPlanet: PlayerPlanet, val expeditionFleet: 
       fleetMissionType = Expedition,
       speed = FleetSpeed.Percent100,
       resources = FleetResources.Given(Resources.Zero),
-      ships = SendFleetRequestShips.Ships(ships = getExpeditionFleet(ogame))
+      ships = SendFleetRequestShips.Ships(ships = expeditionFleetHelper.getExpeditionFleet(ogame, startPlanet))
     )
   }
 
-  def getExpeditionFleet(ogame: OrdonOgameDriver): Map[ShipType, Int] = selectShips(ogame.readFleetPage(startPlanet.id))
-
   override def toString: String = s"Expedition $resumeOn"
+}
+
+class ExpeditionFleetHelper {
+  private val maxLargeCargoCount = 1100
+  private val masExplorerCount = 100
+
+  def getExpeditionFleet(ogame: OrdonOgameDriver, startPlanet: PlayerPlanet): Map[ShipType, Int] = {
+    val myFleetPageData = ogame.readMyFleets()
+    val fleetPageData = ogame.readFleetPage(startPlanet.id)
+    val availableExpeditionShips = (myFleetPageData.fleets
+      .filter(f => f.fleetMissionType == Expedition)
+      .flatMap(_.ships.toList) ++ fleetPageData.ships.toList)
+      .groupBy(e => e._1)
+      .map(e => e._1 -> e._2.map(_._2).sum)
+    val averageExpeditionShips = availableExpeditionShips
+      .map(e => e._1 -> e._2 / myFleetPageData.fleetSlots.maxExpeditions)
+    Map(
+      LightFighter -> averageExpeditionShips(LightFighter),
+      LargeCargoShip -> Math.min(averageExpeditionShips(LargeCargoShip), maxLargeCargoCount),
+      Explorer -> Math.min(averageExpeditionShips(Explorer), masExplorerCount),
+      Destroyer -> 1,
+      EspionageProbe -> 1
+    )
+  }
 }
