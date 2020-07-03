7bf84944c176553059534a825f3c87c818d96d1b
Karol Kowalski <ordon1313@gmail.com>
2020-07-03 18:42:25 +0200

    [Ordon] [Refactor] Remove usage of SendFleet from expedition action

--- ordon/src/main/scala/not/ogame/bots/ordon/action/ExpeditionOrdonAction.scala
+++ ordon/src/main/scala/not/ogame/bots/ordon/action/ExpeditionOrdonAction.scala
@@ -6,20 +6,10 @@ import not.ogame.bots.CoordinatesType.Planet
 import not.ogame.bots.FleetMissionType.Expedition
 import not.ogame.bots._
 import not.ogame.bots.ordon.core.{EventRegistry, ExpeditionFleetChanged, OrdonOgameDriver, TimeBasedOrdonAction}
-import not.ogame.bots.ordon.utils.{SelectResources, SelectShips, SendFleet}
+import not.ogame.bots.ordon.utils.SelectShips
 
 class ExpeditionOrdonAction(val startPlanet: PlayerPlanet, val expeditionFleet: Map[ShipType, Int]) extends TimeBasedOrdonAction {
-  private val coordinates: Coordinates = startPlanet.coordinates.copy(position = 16, coordinatesType = Planet)
   private val selectShips: SelectShips = page => page.ships.map(e => e._1 -> Math.min(e._2, expeditionFleet.getOrElse(e._1, 0)))
-  private val selectResources: SelectResources = _ => Resources.Zero
-  val sendFleetHelper = new SendFleet(
-    from = startPlanet,
-    to = PlayerPlanet(PlanetId.apply(""), coordinates),
-    selectShips = selectShips,
-    selectResources = selectResources,
-    missionType = Expedition,
-    fleetSpeed = FleetSpeed.Percent100
-  )
 
   override def processTimeBased(ogame: OrdonOgameDriver, eventRegistry: EventRegistry): ZonedDateTime = {
     val page = ogame.readMyFleets()
@@ -28,10 +18,23 @@ class ExpeditionOrdonAction(val startPlanet: PlayerPlanet, val expeditionFleet: 
       eventRegistry.registerEvent(ExpeditionFleetChanged(nextExpeditionFleet.arrivalTime.plusSeconds(3), nextExpeditionFleet.to))
       nextExpeditionFleet.arrivalTime
     } else {
-      ogame.sendFleet(sendFleetHelper.getSendFleetRequest(ogame))
+      ogame.sendFleet(getSendFleetRequest(ogame))
       processTimeBased(ogame, eventRegistry)
     }
   }
+
+  private def getSendFleetRequest(ogame: OrdonOgameDriver): SendFleetRequest = {
+    SendFleetRequest(
+      from = startPlanet,
+      targetCoordinates = startPlanet.coordinates.copy(position = 16, coordinatesType = Planet),
+      fleetMissionType = Expedition,
+      speed = FleetSpeed.Percent100,
+      resources = FleetResources.Given(Resources.Zero),
+      ships = SendFleetRequestShips.Ships(ships = getExpeditionFleet(ogame))
+    )
+  }
+
+  def getExpeditionFleet(ogame: OrdonOgameDriver): Map[ShipType, Int] = selectShips(ogame.readFleetPage(startPlanet.id))
 
   override def toString: String = s"Expedition $resumeOn"
 }
