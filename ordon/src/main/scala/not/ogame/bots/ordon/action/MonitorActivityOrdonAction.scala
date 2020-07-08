ddd4687ed2c0bf234c343bd88679d4dd32570142
Karol Kowalski <ordon1313@gmail.com>
2020-07-08 22:58:51 +0200

    [Ordon] Preety print monitor status

--- ordon/src/main/scala/not/ogame/bots/ordon/action/MonitorActivityOrdonAction.scala
+++ ordon/src/main/scala/not/ogame/bots/ordon/action/MonitorActivityOrdonAction.scala
@@ -2,9 +2,10 @@ package not.ogame.bots.ordon.action
 
 import java.time.ZonedDateTime
 
+import not.ogame.bots.PlayerActivity.LessThan15MinutesAgo
 import not.ogame.bots.ordon.core.{EventRegistry, OrdonOgameDriver, TimeBasedOrdonAction}
 import not.ogame.bots.ordon.utils.SlackIntegration
-import not.ogame.bots.{Coordinates, PlayerPlanet}
+import not.ogame.bots.{Coordinates, PlayerActivity, PlayerPlanet}
 
 class MonitorActivityOrdonAction(monitoredPlayerName: String, playerPlanet: PlayerPlanet, coordinatesList: List[Coordinates])
     extends TimeBasedOrdonAction {
@@ -22,9 +23,16 @@ class MonitorActivityOrdonAction(monitoredPlayerName: String, playerPlanet: Play
       })
       .toList
       .sortBy(entry => entry._1.galaxy * 100000 + entry._1.system * 100 + entry._1.position)
-      .map(entry => entry.toString())
+      .map(entry => {
+        entryToString(entry)
+      })
       .mkString(s"$monitoredPlayerName\n", "\n", "\n")
     slackIntegration.postActivityMessageToSlack(message)
     ZonedDateTime.now().plusMinutes(10)
+  }
+
+  private def entryToString(entry: (Coordinates, PlayerActivity)) = {
+    val active = if (entry._2 == LessThan15MinutesAgo) "\t\t\tActive" else entry._2.toString
+    s"${entry._1.galaxy}:${entry._1.system}:${entry._1.position}\t${entry._1.coordinatesType}\t$active"
   }
 }
