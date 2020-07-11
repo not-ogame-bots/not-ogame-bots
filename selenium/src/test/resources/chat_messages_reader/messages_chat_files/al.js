a95a9a52caa4baa3080d6c69af75362aef9d75f5
Karol Kowalski <ordon1313@gmail.com>
2020-07-12 00:46:01 +0200

    [Selenium] Add messages_chat.html

---
+++ selenium/src/test/resources/chat_messages_reader/messages_chat_files/al.js
@@ -0,0 +1,3 @@
+var MAX_af0eb243 = '';
+MAX_af0eb243 += "<"+"div id=\'beacon_dc01e97001\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'https://ads-delivery.gameforge.com/lg.php?bannerid=0&amp;campaignid=0&amp;zoneid=1331&amp;source=Quelle&amp;loc=https%3A%2F%2Fs169-pl.ogame.gameforge.com%2Fgame%2Findex.php%3Fpage%3Dchat&amp;cb=dc01e97001\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
+document.write(MAX_af0eb243);
